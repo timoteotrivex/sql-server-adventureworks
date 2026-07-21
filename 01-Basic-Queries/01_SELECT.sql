b248d87f94ec7c557d71d55cc5bd09f9a36c87c7
Timoteo <timotrive@hotmail.com>
2026-07-21 12:40:01 -0300

    Create 01_SELECT.sql

---
+++ 01-Basic-Queries/01_SELECT.sql
@@ -0,0 +1,13 @@
+PROYECTO A GUARDAR 
+
+
+1- SELECT  * FROM [Person].[Person]; 
+
+/*=========================================================
+CONSULTA 1 - SELECT *
+Objetivo:
+Mostrar todos los registros y columnas de la tabla Person.Person.
+=========================================================*/
+
+SELECT *
+FROM Person.Person;
