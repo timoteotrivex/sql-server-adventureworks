b6ceff93cef761a73e5db262c1720fec9d1c9956
Timoteo <timotrive@hotmail.com>
2026-07-23 16:15:21 -0300

    Update Consulta_03_ALIAS.sql

--- 01_SELECT/Consulta_03_ALIAS.sql
+++ 01_SELECT/Consulta_03_ALIAS.sql
@@ -16,3 +16,47 @@ SELECT
     FirstName AS Nombre,
     LastName AS Apellido
 FROM Person.Person;
+
+
+
+------------------------------------------------------------------------------------
+
+CONSULTA 4 - Alias de columnas sin AS
+
+Objetivo:
+Aprender que SQL Server permite crear alias
+sin utilizar la palabra AS.
+
+Concepto:
+AS es opcional para alias de columnas.
+=========================================================*/
+
+SELECT
+    BusinessEntityID ID,
+    FirstName Nombre,
+    LastName Apellido
+FROM Person.Person;
+
+--------------------------------------------------------------------------------------
+
+
+CONSULTA 5 - Alias con espacios
+
+Objetivo:
+Aprender a crear alias con nombres compuestos.
+
+Conceptos aprendidos:
+- AS
+- Alias con espacios
+- Corchetes []
+
+Buenas prácticas:
+Utilizar nombres amigables para reportes y dashboards.
+=========================================================*/
+
+SELECT
+    BusinessEntityID AS [ID Cliente],
+    FirstName AS Nombre,
+    LastName AS Apellido
+FROM Person.Person;
+
