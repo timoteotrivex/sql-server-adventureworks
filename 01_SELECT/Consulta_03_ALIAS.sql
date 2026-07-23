14dcb7bad42768ad73ed6f1cd109f3c96661318a
Timoteo <timotrive@hotmail.com>
2026-07-23 16:12:11 -0300

    Create Consulta_03_ALIAS.sql

---
+++ 01_SELECT/Consulta_03_ALIAS.sql
@@ -0,0 +1,18 @@
+CONSULTA 3 - Alias de columnas
+
+Objetivo:
+Mostrar nombres amigables para los encabezados del reporte.
+
+Conceptos aprendidos:
+- AS
+- Alias de columnas
+
+Nivel:
+Básico
+=========================================================*/
+
+SELECT
+    BusinessEntityID AS ID,
+    FirstName AS Nombre,
+    LastName AS Apellido
+FROM Person.Person;
